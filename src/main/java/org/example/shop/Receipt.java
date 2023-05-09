cb594f58dfa45d7cc2e03435ce850b5f4ba0c9db
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-05-09 23:15:52 +0300

    creating class checkout so that we can track them

---
+++ src/main/java/org/example/shop/Receipt.java
@@ -0,0 +1,12 @@
+package org.example.shop;
+
+import org.example.shop.goods.Cashiers;
+import org.example.shop.goods.Shop;
+import org.example.shop.goods.goods.Goods;
+
+public class Receipt {
+    private Shop shop;
+    private Cashiers cashier;
+    private Chekout chekout;
+    private Goods goods;
+}
